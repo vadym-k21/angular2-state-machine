bd150d0cce96a6d46ea22aa0a5c5f38449f09f78
vkukhtin <vadym.kukhtin@zalando.de>
2016-11-06 21:39:30 +0100

    Add correct example and README

--- core.spec.ts
+++ core.spec.ts
@@ -1,7 +1,7 @@
 /**
  * Created by vkukhtin on 08/08/16.
  */
-import {StateMachine, StateEvent} from '../../../dist/stateMachine';
+import {StateMachine, StateEvent} from './core';
 
 let fsm = new StateMachine({
     initial: 'green',
